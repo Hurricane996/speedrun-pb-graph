58010c23b8d722b635eca64f9eca8e44ea66305f
Willrich, Levi <lwillrich@hy-vee.com>
2021-07-16 13:19:42 -0500

    refactored out sorting function

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,9 +2,8 @@ import React, {  FC } from "react";
 import {Link, useParams} from "react-router-dom";
 import { ErrorAlert, LoadingAlert } from "../components/Alerts";
 import { SPEEDRUN_COM_URL } from "../App";
-import { SRCResult, SRCUser, EmbedGame, EmbedCategory, EmbedLevel, SRCPB } from "../types/SRCQueryResults";
-import useFetcher, { Fetcher } from "../utils/useFetcher";
-import loadVariables from "../utils/loadVariables";
+import { SRCResult, SRCUser, EmbedGame, EmbedCategory, EmbedLevel, SRCPB, SRCVariableSet, SRCVariable } from "../types/SRCQueryResults";
+import useFetcher, { Fetcher, FetchWrapper } from "../utils/useFetcher";
 
 interface Game {
     name: string;
@@ -26,17 +25,43 @@ interface LevelCategory extends Category {
     levelName: string;
 }
 
+interface UserData {
+    id: string;
+    name: string;
+    games: Game[];
+}
+
 export interface Subcategory {
     subcategoryKeyId: string;
     subcategoryValueId: string;
     subcategoryValueName: string;
 }
 
-interface UserData {
-    id: string;
-    name: string;
-    games: Game[];
-}
+const loadVariables = async (variables: SRCVariableSet, fetchWrapper: FetchWrapper): Promise<Subcategory[]> =>  
+    await Promise.all(Object.entries(variables).map(async ([key, value]: [string, string]) => {
+        const variableData = await fetchWrapper<SRCResult<SRCVariable>>(`${SPEEDRUN_COM_URL}/variables/${key}`);
+
+        return {
+            subcategoryKeyId: key,
+            subcategoryValueId: value,
+            subcategoryValueName: variableData.data.values.values[value].label
+        };
+    }));
+
+const sortCategories = (a: Category|LevelCategory, b: Category|LevelCategory) => {
+    if(a.categoryName < b.categoryName) return -1;
+    if(a.categoryName > b.categoryName) return 1;
+    if(a.subcategories.length === 0 || b.subcategories.length === 0) return 0;
+    if(a.subcategories[0].subcategoryValueName < b.subcategories[0].subcategoryValueName) return -1;
+    if(a.subcategories[0].subcategoryValueName > b.subcategories[0].subcategoryValueName) return 1;
+    if("levelName" in a && "levelName" in b) {
+        if(a.levelName < b.levelName) return - 1;
+        if(a.levelName > b.levelName) return 1;
+    }
+    return 0;
+};
+
+
 
 const fetcher: Fetcher<{id: string},UserData> = async ({id}, fetchWrapper) => {
     const [userApiData, pbData] = await Promise.all([
@@ -72,30 +97,12 @@ const fetcher: Fetcher<{id: string},UserData> = async ({id}, fetchWrapper) => {
     const games: Game[] = gameIds.map(id => {
         const fullGameCategories = categoryDataFullGame
             .filter(category => category.gameId === id)
-            .sort((a: Category, b: Category) => {
-                if(a.categoryName < b.categoryName) return -1;
-                if(a.categoryName > b.categoryName) return 1;
-                if(a.subcategories.length === 0 || b.subcategories.length === 0) return 0;
-                if(a.subcategories[0].subcategoryValueName < b.subcategories[0].subcategoryValueName) return -1;
-                if(a.subcategories[0].subcategoryValueName > b.subcategories[0].subcategoryValueName) return 1;
-                return 0;
-            });
+            .sort(sortCategories);
 
 
         const levelCategories = categoryDataILs
             .filter(category => category.gameId === id)
-            .sort((a: LevelCategory, b: LevelCategory) => {
-                if(a.levelName < b.levelName) return -1;
-                if(a.levelName > b.levelName) return 1;
-                if(a.categoryName < b.categoryName) return -1;
-                if(a.categoryName > b.categoryName) return 1;
-                if(a.subcategories.length === 0 || b.subcategories.length === 0) return 0;
-                if(a.subcategories[0].subcategoryValueName < b.subcategories[0].subcategoryValueName) return -1;
-                if(a.subcategories[0].subcategoryValueName > b.subcategories[0].subcategoryValueName) return 1;
-
-                return 0;
-            });
-
+            .sort(sortCategories);
         return {
             id,
             name: fullGameCategories.length > 0 ? fullGameCategories[0].gameName : levelCategories[0].gameName,
